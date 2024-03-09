2fe0b0a96d2f22b87cab3581d4b7a251abeb9d30
Eddie Brown <edmundvwbrown@gmail.com>
2024-03-09 17:56:42 +0000

    First Commit

---
+++ lua/eddie/packer.lua
@@ -0,0 +1,44 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = {{'nvim-lua/plenary.nvim'} }
+	}
+	use { "catppuccin/nvim", 
+	as = "catppuccin",
+	config = function()
+		vim.cmd('colorscheme catppuccin-macchiato')
+	end
+}
+use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+use('nvim-treesitter/playground')
+use('theprimeagen/harpoon')
+use('mbbill/undotree')
+use('tpope/vim-fugitive')
+
+use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lua'},
+
+    {'L3MON4D3/LuaSnip'},     -- Required
+    {'rafamadriz/friendly-snippets'}
+  }
+}
+end)
